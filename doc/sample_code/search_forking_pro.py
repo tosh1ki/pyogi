dbcd62197741239c3a3d91a93eceb3f9c8d2e8e6
tosh1ki <tosh1ki@yahoo.co.jp>
2015-06-21 11:21:32 +0900

    Add search_forking_pro.py

---
+++ doc/sample_code/search_forking_pro.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.append('./../../')
+
+from pyogi.ki2converter import *
+from pyogi.kifu import *
+
+
+if __name__ == '__main__':
+    
+    for n in range(0, 50000):
+
+        n1 = (n // 10000)
+        n2 = int(n < 10000)
+    
+        relpath = '~/data/shogi/2chkifu/{0}000{1}/{2:0>5}.KI2'.format(n1, n2, n)
+        kifile = os.path.expanduser(relpath)
+
+        if not os.path.exists(kifile):
+            continue
+            
+        ki2converter = Ki2converter()
+        ki2converter.from_path(kifile)
+
+        csa = ki2converter.to_csa()
+        kifu = Kifu(csa)
+        kifu.get_forking(['OU', 'HI'])
+        print()
+
+        break
