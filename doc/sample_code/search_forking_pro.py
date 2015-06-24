65431ea379bf693985a6cfbffdebeab5ffcc9f16
tosh1ki <tosh1ki@yahoo.co.jp>
2015-06-25 00:01:20 +0900

    Refactor search_forking_pro.py

--- doc/sample_code/search_forking_pro.py
+++ doc/sample_code/search_forking_pro.py
@@ -4,10 +4,14 @@
 '''
 プロ棋士の棋譜の中から王手飛車取りの場面を探して，
 「プロの対局では王手飛車をかけたほうが負ける」が本当かどうかを確かめる
+
+2chkifu.zipをダウンロードして適当なディレクトリに解凍，
+`python3 search_forking_pro.py -p [2chkifuのパス]` のような感じで実行する
 '''
 
 import os
 import sys
+import argparse
 import pandas as pd
 sys.path.append('./../../')
 
@@ -17,21 +21,31 @@ from pyogi.kifu import *
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path_2chkifu',
+                        default='~/data/shogi/2chkifu/',
+                        help='2chkifu.zipを展開したディレクトリ')
+    args = parser.parse_args()
+    path_2chkifu = args.path_2chkifu
+
+    sub_dir_list = ['00001', '10000', '20000', '30000', '40000']
+    path_ki2_list = []
+
+    # Extract paths of KI2 files
+    for sub_dir in sub_dir_list:
+        path_dir = os.path.expanduser(os.path.join(path_2chkifu, sub_dir))
+        ki2files = os.listdir(path_dir)
+
+        for ki2file in ki2files:
+            path_ki2_list.append(os.path.join(path_dir, ki2file))
+
+
     res_table = []
     
-    for n in range(0, 50000):
-
-        n1 = (n // 10000)
-        n2 = int(n < 10000)
-    
-        path = '~/data/shogi/2chkifu/{0}000{1}/{2:0>5}.KI2'.format(n1, n2, n)
-        kifile = os.path.expanduser(path)
-
-        if not os.path.exists(kifile):
-            continue
+    for path_ki2 in path_ki2_list:
             
         ki2converter = Ki2converter()
-        ki2converter.from_path(kifile)
+        ki2converter.from_path(path_ki2)
 
         csa = ki2converter.to_csa()
 
@@ -52,7 +66,7 @@ if __name__ == '__main__':
         #   forkandwin: (sente won & sente forked) | (gote won & gote forked)
         res_table.append(
             {
-                'n': n,
+                'path': path_ki2,
                 'player0': kifu.players[0],
                 'player1': kifu.players[1],
                 'sente_won': kifu.sente_win,
