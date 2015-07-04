68ef11605456b64a2492880f0f5d3aa2da3e8bf5
tosh1ki <tosh1ki@yahoo.co.jp>
2015-07-04 23:23:36 +0900

    Rename variables

--- doc/sample_code/use_shogikakolog/extract_kifu.py
+++ doc/sample_code/use_shogikakolog/extract_kifu.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
         html = re.sub('\n', '', html)
 
-        kakikomis = extract_kifutxt(html)
+        kakikomis = extract_kakikomitxt(html)
         kakikomi_list.extend(kakikomis)
         print(filename, len(kakikomis))
 
@@ -35,28 +35,53 @@ if __name__ == '__main__':
 
 
     kifu_list = []
+    prev_converter = None
+    prev_extracted = True
+
     for kakikomi in kakikomi_list:
         txt = re.sub('<br> ', '\n', kakikomi)
         txt = re.sub('^.+?<DD[^>]+?>', '', txt)
         txt = re.sub('<A[^>]+?>', '', txt)
-        
+        txt = re.search('(開始日時：.+?手で[先後]手の勝ち)', txt, re.S)
+
+        # If there is NOT strings of kifu like KI2 format
+        if not txt:
+            prev_extracted = False
+            prev_converter = None
+            continue
+        else:
+            txt = txt.group()
+
         converter = Ki2converter()
         converter.from_txt(txt)
 
         if not converter.moves_ki2:
+            prev_extracted = False
+            prev_converter = None
             continue
+
+        if not prev_extracted and not txt.startswith('開始日時'):
+            prev_converter.extend(converter.moves_ki2)
+            converter = prev_converter
 
         try:
             csa = converter.to_csa()
         except UnicodeDecodeError:
             print(kakikomi)
-
             continue
 
         if csa:
             kifu = Kifu(csa)
             kifu_list.append(kifu)
 
+            kifu.print_state()
+            prev_extracted = True
+        else:
+            print(txt)
+            prev_extracted = False
+
         n_kifu = len(kifu_list)
         if n_kifu%1000 == 0:
             print(n_kifu)
+
+        prev_converter = converter
