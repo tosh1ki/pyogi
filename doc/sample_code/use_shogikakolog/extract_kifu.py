ab9c9bdb3950c59ba6b321762ed35bd70ae9b73f
tosh1ki <tosh1ki@yahoo.co.jp>
2015-07-04 22:42:09 +0900

    Modify for debug

--- doc/sample_code/use_shogikakolog/extract_kifu.py
+++ doc/sample_code/use_shogikakolog/extract_kifu.py
@@ -10,13 +10,16 @@ from pyogi.kifu import Kifu
 from pyogi.threadcrawler import extract_kifutxt
 from pyogi.ki2converter import Ki2converter
 
+import pdb
+
 
 if __name__ == '__main__':
 
     save_dir = os.path.expanduser('~/data/shogi/threads/')
     file_list = os.listdir(save_dir)
+    kakikomi_list = []
 
-    for filename in sorted(file_list):
+    for filename in sorted(file_list, reverse=True):
         filepath = os.path.join(save_dir, filename)
     
         with open(filepath, 'r') as f:
@@ -24,15 +27,16 @@ if __name__ == '__main__':
 
         html = re.sub('\n', '', html)
 
-        kakikomi_list = extract_kifutxt(html)
-        print(filename, len(kakikomi_list))
+        kakikomis = extract_kifutxt(html)
+        kakikomi_list.extend(kakikomis)
+        print(filename, len(kakikomis))
 
         break
 
 
     kifu_list = []
     for kakikomi in kakikomi_list:
-        txt = re.sub('<BR>', '\n', kakikomi)
+        txt = re.sub('<br> ', '\n', kakikomi)
         txt = re.sub('^.+?<DD[^>]+?>', '', txt)
         txt = re.sub('<A[^>]+?>', '', txt)
         
@@ -44,11 +48,15 @@ if __name__ == '__main__':
 
         try:
             csa = converter.to_csa()
-        except (KeyError, AttributeError, ValueError):
+        except UnicodeDecodeError:
+            print(kakikomi)
+
             continue
 
         if csa:
             kifu = Kifu(csa)
-            kifu.print_state(mode='mpl')
-
             kifu_list.append(kifu)
+
+        n_kifu = len(kifu_list)
+        if n_kifu%1000 == 0:
+            print(n_kifu)
