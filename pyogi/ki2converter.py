89da2dc90e992f4ae7254dfb14becac92aa097c2
tosh1ki <tosh1ki@yahoo.co.jp>
2015-07-20 23:09:28 +0900

    Delete TEBAN_CODE from ki2converter.py

--- pyogi/ki2converter.py
+++ pyogi/ki2converter.py
@@ -3,11 +3,11 @@
 
 import re
 
-from .board import Board, TEBAN_CODE
+from .board import Board
 from .pieces_act import PIECE_TO_ACT, TURN_PIECE_REVERSED, KANJI_TO_PIECE, TEAITXT_TO_TEAI, KOMAOCHI_CODE_TO_CSA, KOMA_INFOS
 
 
-SYMBOL_TO_CODE = {'▲': TEBAN_CODE[0], '△': TEBAN_CODE[1]}
+SYMBOL_TO_CODE = {'▲': '+', '△': '-'}
 REGEX_MOVE = re.compile('([▲△](?:同\u3000)?[^▲△\s]+)')
 REGEX_KI2_MOVE = re.compile('([▲△])(同|[\d一二三四五六七八九]{2})(.)(.+)?')
 OGOMA = KOMA_INFOS.query('ogoma == True').csa.pipe(list)
