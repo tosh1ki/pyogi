668beed0315e6ef5a816e17d5c8b81210aa5ac84
tosh1ki <tosh1ki@yahoo.co.jp>
2015-07-26 10:22:32 +0900

    Fix koma.pyx

--- pyogi/koma.pyx
+++ pyogi/koma.pyx
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from .pieces_act import KOMA_INFOS
 
 from cpython cimport bool as bool_t
+
+CSA_TO_INFO = KOMA_INFOS.set_index('csa').T.to_dict()
+KOMA_CSA_ALL = list(KOMA_INFOS.csa)
 
 
 cdef class Koma:
@@ -16,24 +16,25 @@ cdef class Koma:
     '''
 
     cdef:
-        str kanji, kanji_rear, csa, csa_rear
-        bool_t is_promoted
+        readonly str kanji, kanji_rear, csa, csa_rear
+        readonly bool_t is_promoted
+        readonly list act
 
     def __init__(self, piece_type):
-        if piece_type not in list(KOMA_INFOS.csa):
+        if piece_type not in KOMA_CSA_ALL:
             raise RuntimeError('Invalid piece_type:', piece_type)
 
-        info = KOMA_INFOS.query('csa == @piece_type').iloc[0, :]
-        self.csa = info.csa
-        self.kanji = info.kanji
-        self.kanji_rear = info.kanji_rear
-        self.is_promoted = bool(info.promoted)
-        self.act = info.act
+        info = CSA_TO_INFO[piece_type]
+        self.csa = piece_type
+        self.kanji = info['kanji']
+        self.kanji_rear = info['kanji_rear']
+        self.is_promoted = info['promoted']
+        self.act = info['act']
 
         if self.is_promoted:
-            self.csa_rear = info.beforepromote
+            self.csa_rear = info['beforepromote']
         else:
-            self.csa_rear = info.afterpromote
+            self.csa_rear = info['afterpromote']
 
     def __str__(self):
         return self.csa
@@ -42,7 +43,7 @@ cdef class Koma:
         self.csa, self.csa_rear = self.csa_rear, self.csa
         self.kanji, self.kanji_rear = self.kanji_rear, self.kanji
 
-    cdef void promote(self):
+    def promote(self):
         '''Promote this piece
         '''
         if not self.is_promoted:
@@ -51,7 +52,7 @@ cdef class Koma:
         else:
             raise RuntimeError('This piece is already promoted.')
 
-    cdef void depromote(self):
+    def depromote(self):
         '''Return promoted piece
         '''
         if self.is_promoted:
