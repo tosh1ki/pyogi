9a9508ea0d8f275070603a8238e9cab26a41a6d1
tosh1ki <tosh1ki@yahoo.co.jp>
2015-08-24 01:04:45 +0900

    Change test settings

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -4,8 +4,10 @@ cd ./ci
 python convert_ki2.py
 cd -
 cd ./doc/sample_code/
-python demo_plot_state_mpl.py
+pwd
+python demo_plot_state.py
 python search_forking_pro.py -p ./../../data/2chkifu/
 python test_komaochi_convert.py -p ./../../data/2chkifu/
 python demo_kifuconvert.py -p ./../../data/2chkifu/
 cd -
+pwd
