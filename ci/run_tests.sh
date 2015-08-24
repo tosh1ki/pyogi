98cddbf39aed989e49c46b3984f4663fee1425fb
tosh1ki <tosh1ki@yahoo.co.jp>
2015-08-24 10:15:27 +0900

    Change test settings

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -5,10 +5,9 @@ bash 2chkifu.sh
 python convert_ki2.py
 cd -
 cd ./doc/sample_code/
-pwd
 python search_forking_pro.py -p ./../../data/2chkifu/
-python test_komaochi_convert.py -p ./../../data/2chkifu/
+python test_komaochi_convert.py -p ./../../data/2chkifu/ > results_komaochi_convert.txt
 python demo_kifuconvert.py -p ./../../data/2chkifu/ > results_kifu_convert.txt
+tail results_komaochi_convert.txt
 tail results_kifu_convert.txt
 cd -
-pwd
