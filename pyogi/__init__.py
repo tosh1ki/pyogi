e151ad143f1d461ea0595e3ea9af42243901fc87
tosh1ki <tosh1ki@yahoo.co.jp>
2015-08-30 00:01:04 +0900

    Delete import in __init__.py

--- pyogi/__init__.py
+++ pyogi/__init__.py
@@ -1,9 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from .pieces_act import *
-from .koma import Koma
-from .grid import Grid
-from .board import Board
-from .kifu import Kifu
-from .warscrawler import WarsCrawler
