9b8186416ce76b756470239b75662e3ddd18eeb7
tosh1ki <tosh1ki@yahoo.co.jp>
2015-08-29 22:58:52 +0900

    Enable `python3 setup.py install`

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from setuptools import setup, Extension
 
-setup(ext_modules=cythonize('pyogi/*.pyx'))
 
+pyx_list = [
+    ('koma', 'pyx'),
+    ('grid', 'pyx'),
+    ('board', 'pyx'),
+    ('kifu', 'pyx'),
+    ('ki2converter', 'pyx'),
+    ('plot', 'py'),
+    ('threadcrawler', 'py'),
+    ('warscrawler', 'py')
+]
+
+ext_modules = []
+
+for name, ex in pyx_list:
+    ex = Extension(
+        'pyogi.{0}'.format(name),
+        sources=['pyogi/{0}.{1}'.format(name, ex)]
+    )
+
+    ext_modules.append(ex)
+
+
+setup(
+    name='pyogi',
+    packages=['pyogi'],
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext}
+)
