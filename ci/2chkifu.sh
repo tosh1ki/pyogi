14822141f0cb665ef9cb8e1e2f2203fd644067ae
tosh1ki <tosh1ki@yahoo.co.jp>
2015-08-29 23:15:58 +0900

    Make 2chkifu.sh not to download kifu files if exists

--- ci/2chkifu.sh
+++ ci/2chkifu.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 cd ./../data/
-wget https://zipkifubrowser.googlecode.com/files/2chkifu.zip
-unzip -q 2chkifu.zip
+
+if [ ! -f 2chkifu.zip ]; then
+    wget https://zipkifubrowser.googlecode.com/files/2chkifu.zip
+fi
+
+if [ ! -d 2chkifu/ ]; then
+    unzip -q 2chkifu.zip
+fi
+
 cd -
