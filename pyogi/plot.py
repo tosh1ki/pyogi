7326ac1682cd1c1e22e22d65b29a4619e432168b
tosh1ki <tosh1ki@yahoo.co.jp>
2015-08-30 10:25:46 +0900

    Split plot from board class

--- pyogi/plot.py
+++ pyogi/plot.py
@@ -4,7 +4,12 @@
 
 import os
 
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+font = {'family': 'TakaoGothic'}
+matplotlib.rc('font', **font)
+
 import matplotlib.image as mpimg
 from matplotlib import gridspec
 
@@ -14,6 +19,8 @@ csa_to_code = {
     'KE': 6, 'KY': 7, 'FU': 8, 'RY': 22, 'UM': 23,
     'NG': 24, 'NK': 26, 'NY': 27, 'TO': 28
 }
+board_indexes = list(range(0, 9))
+
 
 
 class PicPlot:
@@ -108,20 +115,97 @@ class PicPlot:
         plt.show()
 
 
-def plot_state_pic(pieces_list, mochigoma_list, savepath=None):
-    picplot = PicPlot(pieces_list, mochigoma_list)
-    picplot.plot()
+class MplPlot(object):
+    def __init__(self, board, figsize=(8, 9), title='', last_move_xy=None):
+        self.board = board
+        self.title = title
+        self.last_move_xy = last_move_xy
 
-    if savepath:
-        picplot.save(savepath)
+        _, self.ax = plt.subplots(figsize=figsize)
 
-    picplot.show()
+        self.width_x = figsize[0]
+        self.width_y = figsize[1]
+
+        plt.xlim(0, self.width_x)
+        plt.ylim(0, self.width_y)
+
+        self.dx = self.width_x / 9
+        self.dy = self.width_y / 9
+
+        self.fontsize = 30 * min(self.dx, self.dy)
+
+    def plot(self):
+        self.plot_grid()
+        self.plot_pch()
+        self.plot_pieces()
+        self.plot_mochigoma()
+        self.plot_names()
+        self.plot_title(self.title)
+
+    def plot_grid(self):
+        for i in range(1, 9):
+            x = i * self.dx
+            y = i * self.dy
+            plt.plot([0, self.width_x], [y, y], color='black')
+            plt.plot([x, x], [0, self.width_y], color='black')
+
+    def plot_pch(self):
+        for x in [3 * self.dx, 6 * self.dx]:
+            for y in [3 * self.dy, 6 * self.dy]:
+                plt.plot([x], [y],
+                         marker='o', color='black', linestyle='None')
+
+    def plot_pieces(self):
+        for j in board_indexes:
+            for i, b_i in enumerate(board_indexes):
+                grid = self.board[b_i][j]
+
+                x = ((8 - i) + 0.5) * self.dx
+                y = ((8 - j) + 0.5) * self.dy
+
+                if not grid.is_empty():
+                    is_gote = not grid.is_of_sente()
+
+                    # TOFIX: 60, 80にするとよくわからないけどうまくいく
+                    plt.text(x - self.fontsize / 2 / 60, 
+                             y - self.fontsize / 2 / 80,
+                             grid.koma.kanji,
+                             size=self.fontsize, rotation=180 * is_gote)
+                    
+                # Plot circle around piece moved recently
+                if (self.last_move_xy and
+                    len(self.last_move_xy) == 2 and
+                    self.last_move_xy[0] == i and
+                    self.last_move_xy[1] == j):
+                    circle = Circle(
+                        (x, y), 0.5 * self.dx, facecolor='none',
+                        linewidth=3, alpha=0.5)
+                    self.ax.add_patch(circle)
+
+    def plot_mochigoma(self):
+        plt.text(0, -0.5 * self.dx, self.board.get_mochigoma_str(0),
+                 fontsize=self.fontsize)
+        plt.text(0,  9.2 * self.dy, self.board.get_mochigoma_str(1),
+                 fontsize=self.fontsize, rotation=180)
+
+    def plot_names(self):
+        plt.text(self.width_x + 0.2, 2 * self.dy, self.board.players[0],
+                 fontsize=self.fontsize, rotation=90)
+        plt.text(self.width_x + 0.2, 8 * self.dy, self.board.players[1],
+                 fontsize=self.fontsize, rotation=90)
+
+    def plot_title(self, title):
+        plt.title(title, y=1.07, fontsize=self.fontsize)
+        plt.tick_params(labelleft='off', labelbottom='off')
+
+    def save(self, savepath):
+        plt.savefig(savepath)
+    
+    def show(self):
+        plt.show()
 
 
-def plot_board(board, savepath=None, mode='pic'):
-    '''Plot board object
-    '''
-
+def plot_state_pic(board, savepath=None):
     pieces_list = []
     for i, row in enumerate(board.board):
         for j, grid in enumerate(row):
@@ -139,7 +223,30 @@ def plot_board(board, savepath=None, mode='pic'):
             mochigoma_list.append((which_player, _mochigoma.csa))
 
 
+    picplot = PicPlot(pieces_list, mochigoma_list)
+    picplot.plot()
+
+    if savepath:
+        picplot.save(savepath)
+
+    picplot.show()
+
+def plot_state_mpl(board, savepath=None, title='', last_move_xy=None, figsize=(8, 9)):
+    mplplot = MplPlot(board, figsize=figsize, title=title, last_move_xy=last_move_xy)
+    mplplot.plot()
+
+    if savepath:
+        mplplot.save(savepath)
+
+    mplplot.show()
+
+def plot_board(board, savepath=None, title='', mode='pic', last_move_xy=None, figsize=None):
+    '''Plot board object
+    '''
     if mode == 'pic':
-        plot_state_pic(pieces_list, mochigoma_list, savepath)
+        plot_state_pic(board, savepath)
+    elif mode == 'mpl':
+        plot_state_mpl(board, figsize=figsize, savepath=savepath,
+                       title=title, last_move_xy=last_move_xy)
     else:
         print('Invalid mode : {0}'.format(mode))
